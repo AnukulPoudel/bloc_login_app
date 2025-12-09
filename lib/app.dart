0b56ede099bdf0037a58384c33bdd83d3fa82083
AnukulPoudel <anukulpoudel999@gmail.com>
2025-12-09 13:12:28 +0545

    user_repository setup

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:user_repository/user_repository.dart';
+
+class MyApp extends StatelessWidget {
+  final UserRepository userRepository;
+  
+  const MyApp({super.key, required this.userRepository});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
